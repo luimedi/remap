2b522077e9397533ae2173412381c689d55e6756
Luis Medina <6675716+luimedi@users.noreply.github.com>
2025-11-08 09:03:04 +0100

    Add CastDefault class for default value casting with strict mode

---
+++ src/Cast/CastDefault.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Luimedi\Remap\Cast;
+
+use Attribute;
+use DateTime;
+use DateTimeInterface;
+
+#[Attribute(Attribute::TARGET_PARAMETER)]
+class CastDefault implements CastInterface
+{
+    /**
+     * @param mixed $default The default value to return if the input is null or empty.
+     * @param bool  $strict  If strict is true, only null values will be replaced by the default value. 
+     *                       Otherwise if strict is false, empty values (null, '', 0, false) will be replaced by the default value.
+     */
+    public function __construct(protected mixed $default = null, protected bool $strict = false)
+    {
+    }
+    
+    public function cast(mixed $value): mixed
+    {
+        if ($this->strict) {
+            if (is_null($value) ) {
+                return $this->default;
+            }
+            return $value;
+        }
+
+        if (empty($value)) {
+            return $this->default;
+        }
+        
+        return $value;
+    }
+}
