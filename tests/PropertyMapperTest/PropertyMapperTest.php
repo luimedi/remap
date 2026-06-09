0a01d1f29c760b4201fefec83ccef067b1d250fb
Luis Medina <6675716+luimedi@users.noreply.github.com>
2026-06-09 09:04:26 +0200

    Improve mapper related tests

--- tests/PropertyMapperTest/PropertyMapperTest.php
+++ tests/PropertyMapperTest/PropertyMapperTest.php
@@ -20,4 +20,26 @@ class PropertyMapperTest extends TestCase
         $this->assertInstanceOf(Output::class, $output);
         $this->assertSame('2000-01-01T00:00:00+00:00', $output->birthdate);
     }
+
+    public function testPropertyMappingWithCustomDateFormat()
+    {
+        $mapper = new Mapper();
+        $mapper->bind(Input::class, OutputCustomFormat::class);
+
+        $input = new Input();
+        $input->birthdate = new \DateTime('2000-01-01 15:30:00');
+
+        $output = $mapper->map($input);
+
+        $this->assertInstanceOf(OutputCustomFormat::class, $output);
+        $this->assertSame('2000-01-01', $output->birthdate);
+    }
+}
+
+#[\Luimedi\Remap\Attribute\PropertyMapper]
+class OutputCustomFormat
+{
+    #[\Luimedi\Remap\Attribute\MapProperty]
+    #[\Luimedi\Remap\Attribute\Cast\CastDateTime('Y-m-d')]
+    public string $birthdate;
 }
